ba1981e647cb8479f73f96d0f0f9b9ef94b76937
yudnata <yudhinata04@gmail.com>
2025-10-30 19:42:58 +0800

    updates

--- frontend/src/components/SentimentForm.jsx
+++ frontend/src/components/SentimentForm.jsx
@@ -15,7 +15,7 @@ const SentimentForm = ({ onAnalyze, isLoading }) => {
       className="bg-white border border-gray-200 p-10 rounded-3xl shadow-md flex flex-col gap-6 transition-all duration-300 h-[450px]"
     >
       <h2 className="text-2xl font-semibold text-gray-800">Analisis Sentimen</h2>
-      <label className="text-lg font-medium text-gray-600">Masukkan teks opini:</label>
+      <label className="text-lg font-medium text-gray-600">Masukkan Ulasan Produk:</label>
 
       <textarea
         className="w-full p-4 rounded-xl bg-gray-50 border border-gray-300 text-gray-800 resize-none focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-transparent transition-all text-base"
@@ -32,7 +32,7 @@ const SentimentForm = ({ onAnalyze, isLoading }) => {
         className="bg-green-500 hover:bg-green-600 text-white font-semibold py-3 rounded-xl text-lg shadow-md transition-all disabled:bg-gray-400 disabled:cursor-not-allowed"
         disabled={isLoading} // Tambahkan disabled saat loading
       >
-        {isLoading ? 'Menganalisis...' : 'Analisis Sentimen'}
+        {isLoading ? 'Menganalisis...' : 'Analisis'}
       </button>
     </form>
   );
